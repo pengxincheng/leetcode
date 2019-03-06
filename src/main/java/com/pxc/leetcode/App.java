18f24a74aa9e7960cabb65886f11a9f57524f1cc
彭新成 <pengxincheng@ipaynow.cn>
2019-03-06 20:01:56 +0800

    2019-03-06 第14题

--- src/main/java/com/pxc/leetcode/App.java
+++ src/main/java/com/pxc/leetcode/App.java
@@ -2,12 +2,11 @@ package com.pxc.leetcode;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        System.out.println("2018-12-24".substring(8,10));
     }
 }
