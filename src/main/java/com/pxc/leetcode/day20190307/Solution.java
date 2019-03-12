a1ae1697e600cdf53f7f1113bc5b0d826e5471a8
彭新成 <pengxincheng@ipaynow.cn>
2019-03-12 17:26:20 +0800

    2019-03-12 第20题

---
+++ src/main/java/com/pxc/leetcode/day20190307/Solution.java
@@ -0,0 +1,66 @@
+package com.pxc.leetcode.day20190307;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * @author pengxincheng@ipaynow.cn
+ * @Date: 2019/3/7
+ * @Time 15:12
+ * 示例 1:
+ * <p>
+ * 输入: '()'
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: '()[]{}'
+ * 输出: true
+ * 示例 3:
+ * <p>
+ * 输入: '(]'
+ * 输出: false
+ * 示例 4:
+ * <p>
+ * 输入: '([)]'
+ * 输出: false
+ * 示例 5:
+ * <p>
+ * 输入: '{[]}'
+ * 输出: true
+ */
+public class Solution {
+
+    private HashMap<Character, Character> mappings;
+
+    public Solution() {
+        this.mappings = new HashMap<>();
+        mappings.put(')', '(');
+        mappings.put(']', '[');
+        mappings.put('}', '{');
+    }
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        //如果是左括号压栈 如果是右括号看能否找到对应的左括号
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (mappings.containsKey(c)) {
+                Character top = stack.empty() ? '*' : stack.pop();
+                if(!top.equals(mappings.get(c))){
+                    return false;
+                }
+
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+}
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isValid("(([]){})"));
+
+
+    }
+}
