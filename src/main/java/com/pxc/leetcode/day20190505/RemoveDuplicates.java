e8502b621be1d3bb02a5916565c996114160b7af
彭新成 <pengxincheng@ipaynow.cn>
2019-05-05 17:09:31 +0800

    第26题

---
+++ src/main/java/com/pxc/leetcode/day20190505/RemoveDuplicates.java
@@ -0,0 +1,27 @@
+package com.pxc.leetcode.day20190505;
+
+/**
+ * @author pengxincheng@ipaynow.cn
+ * @Date: 2019/5/5
+ * @Time 16:23
+ */
+public class RemoveDuplicates {
+
+    public int removeDuplicates(int[] nums) {
+
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        new RemoveDuplicates().removeDuplicates(a);
+    }
+}
