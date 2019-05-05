dd7e7396e7eb360370506726a651b6204bd34195
彭新成 <pengxincheng@ipaynow.cn>
2019-05-05 16:20:47 +0800

    递归 斐波那契数列

--- src/main/java/com/pxc/leetcode/A.java
+++ src/main/java/com/pxc/leetcode/A.java
@@ -14,4 +14,25 @@ public class A {
     public A() {
         System.out.println("a的构造方法");
     }
+
+
+    public static void main(String[] args) {
+        String s = " <Currencies ForgeMaterial1=\"777777777\" ForgeMaterial34=\"777777777\" ForgeMaterial3=\"777777777\" AscensionTicket=\"0\" />";
+        System.out.println(replace(s,"ForgeMaterial3","8888888"));
+
+    }
+
+    public static String replace(String s, String key, String newVlaue) {
+
+        StringBuffer stringBuffer = new StringBuffer();
+
+        String arr[] = s.split(" ");
+        for (String s1 : arr) {
+            if(s1.contains("=") && s1.substring(0,s1.lastIndexOf("=")).equals(key)){
+                s1 = key + "=" + "\"" + newVlaue + "\"";
+            }
+            stringBuffer.append(s1).append(" ");
+        }
+        return stringBuffer.toString();
+    }
 }
--- src/main/java/com/pxc/leetcode/B.java
+++ src/main/java/com/pxc/leetcode/B.java
@@ -5,7 +5,7 @@ package com.pxc.leetcode;
  * @Date: 2019/4/24
  * @Time 9:22
  */
-public class B extends A{
+public class B extends A {
 
     static {
         System.out.println("b的静态");
@@ -19,7 +19,29 @@ public class B extends A{
     public static void main(String[] args) {
         //B b = new B();
         //System.out.println("///////////////////");
-        A a = new B();
+        //A a = new B();
         //先执行父类的静态 再执行子类的静态  父类的构造 子类的构造
+
+        System.out.println(sumI(100));
+        System.out.println(f(5));
+    }
+
+    public static int sumI(int i) {
+        if (i == 1) {
+            return i;
+        }
+
+        return i + sumI(i - 1);
+    }
+
+
+    //F(1)=1，F(2)=1, F(n)=F(n-1)+F(n-2)
+    public static int f(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 1;
+
+
+        return f(n - 1) + f(n - 2);
+
     }
 }
