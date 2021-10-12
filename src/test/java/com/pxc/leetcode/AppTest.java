8bbfa7aeb65b6b70796c5e040f80cba94a627329
pengxincheng <pengxincheng@mininglamp.com>
2021-10-12 16:04:55 +0800

    fix: 删除其他代码

--- src/test/java/com/pxc/leetcode/AppTest.java
+++ src/test/java/com/pxc/leetcode/AppTest.java
@@ -1,34 +1,8 @@
 package com.pxc.leetcode;
 
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
-import static org.junit.Assert.assertTrue;
-
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-       // assertTrue(true);
 
-        ArrayList<String> arrayList = new ArrayList<>();
-        LinkedList<String> linkedList = new LinkedList<>(arrayList);
-
-        String type = "0";
-      List<String> arr= Arrays.asList("U5","U6");
-
-       arr.add("U8");
-
-        System.out.println(arr);
-
-    }
 }
